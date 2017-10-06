a5d96336f8da00c5df9d0695bfb734dbe7ef692e
Apoorva Pandey <apandey@tavisca.com>
2017-10-06 18:35:49 +0530

    added support for autosuggest

--- HotelSearchUI/scripts/script.js
+++ HotelSearchUI/scripts/script.js
@@ -1,24 +1,40 @@
 $(document).ready(
 function(){
+    var placeSuggestions;
+    $("#destination").autocomplete({source:new Array()});
     $("#destination").keypress(function(){
         var destinationtext=$("#destination").val();
-        if(destinationtext.length>1)
-            {
+            
                 var requestUrl="http://portal.dev-rovia.com/Services/api/Content/GetAutoCompleteDataGroups?type=city|airport|poi&query="+destinationtext;
-//                $.get(requestUrl).done(function(data){var jqueryXml=$(data);console.log(jqueryXml);});
-                    $.ajax({
+              $.ajax({
                     url:requestUrl,
                     method:'get',
                     data:null,
+                    crossDomain:true,
                     dataType:'jsonp',
                     success:function(json){
-                        var jqueryXml=$(json);
-                        console.log(jqueryXml.text());
-                        jqueryXml.find('').text();}
+                        for(var counter=0;counter<json.length;counter++)
+                        {
+                            var itemsList =json[counter].ItemList;
+                            for(var innercounter=0;innercounter<itemsList.length;innercounter++)
+                                {
+                                    if(counter==0&&innercounter==0)
+                                        {
+                                            placeSuggestions=itemsList[innercounter].Name+',';
+                                        }
+                                    else
+                                    {
+                                        placeSuggestions+=itemsList[innercounter].Name+',';
+                                    }
+                                    
+                                }
+                        }
+                        var suggestionArray=placeSuggestions.split(',');
+                        suggestionArray.pop();
+                        $("#destination").autocomplete("option","source",suggestionArray);
+                    }
                     });
-            }
     })
-    
     $("#checkoutdate").datepicker({
         dateFormat:'dd-mm-yy',
         defaultDate:1,
@@ -39,7 +55,7 @@ function(){
             $("#checkoutdate").datepicker("option","minDate",mincheckoutdate);
             
     }});
-    
+    $("")
 
 
 }
