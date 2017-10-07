6e04e6b23ac925f7a0ddddc4f0676044c14608a2
Apoorva Pandey <apandey@tavisca.com>
2017-10-07 13:28:21 +0530

    changes in autocomplete

--- HotelSearchUI/scripts/script.js
+++ HotelSearchUI/scripts/script.js
@@ -1,40 +1,44 @@
 $(document).ready(
 function(){
-    var placeSuggestions;
-    $("#destination").autocomplete({source:new Array()});
-    $("#destination").keypress(function(){
-        var destinationtext=$("#destination").val();
-            
-                var requestUrl="http://portal.dev-rovia.com/Services/api/Content/GetAutoCompleteDataGroups?type=city|airport|poi&query="+destinationtext;
-              $.ajax({
-                    url:requestUrl,
+    var placeSuggestions;var suggestionArray=new Array();
+    $("#location").autocomplete({source:suggestionArray});
+    $("#location").on('input propertychange',function(){$("#secondaryElementsContainer").css("display","block")});
+    $("#location").on('keyup input propertychange',function(){
+                var locationtext=$("#location").val();
+                var autoSuggestRequestUrl="http://portal.dev-rovia.com/Services/api/Content/GetAutoCompleteDataGroups?type=city|airport|poi&query="+locationtext;
+                $.ajax({
+                    url:autoSuggestRequestUrl,
                     method:'get',
                     data:null,
                     crossDomain:true,
                     dataType:'jsonp',
                     success:function(json){
-                        for(var counter=0;counter<json.length;counter++)
+                        if(json!=null)
                         {
+                            for(var counter=0;counter<json.length;counter++)
+                            {
                             var itemsList =json[counter].ItemList;
                             for(var innercounter=0;innercounter<itemsList.length;innercounter++)
                                 {
                                     if(counter==0&&innercounter==0)
                                         {
-                                            placeSuggestions=itemsList[innercounter].Name+',';
+                                            placeSuggestions=itemsList[innercounter].Name+' '+itemsList[innercounter].CityName+' '+itemsList[innercounter].Id.toString()+',';
                                         }
                                     else
                                     {
-                                        placeSuggestions+=itemsList[innercounter].Name+',';
+                                        placeSuggestions+=itemsList[innercounter].Name+' '+itemsList[innercounter].CityName+' '+itemsList[innercounter].Id.toString()+',';
                                     }
                                     
                                 }
+                            }
+                            suggestionArray=placeSuggestions.split(',');
+                            suggestionArray.pop();
+                            $("#location").autocomplete("option","source",suggestionArray);
                         }
-                        var suggestionArray=placeSuggestions.split(',');
-                        suggestionArray.pop();
-                        $("#destination").autocomplete("option","source",suggestionArray);
+                        
                     }
                     });
-    })
+    });
     $("#checkoutdate").datepicker({
         dateFormat:'dd-mm-yy',
         defaultDate:1,
@@ -55,7 +59,8 @@ function(){
             $("#checkoutdate").datepicker("option","minDate",mincheckoutdate);
             
     }});
-    $("")
+    
+    });
 
 
 }
