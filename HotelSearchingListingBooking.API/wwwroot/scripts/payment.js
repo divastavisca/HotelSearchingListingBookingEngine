06dd0b338735bc6cc0ac82a82acb4725bacf839e
Apoorva Pandey <apandey@tavisca.com>
2017-10-26 16:05:36 +0530

    js scripts for payment and booking transactions with api and updated payment ui

--- HotelSearchingListingBooking.API/wwwroot/scripts/payment.js
+++ HotelSearchingListingBooking.API/wwwroot/scripts/payment.js
@@ -2,13 +2,13 @@
     var sessionId;
     var summaryFieldsPlaceHolders;
     var summaryFieldValues;
-    var guestDetailsHtml = "<fieldset id='contact-details'><h3 id='contact'>Contact Information</h3><div><label for='first name'>First name:<span class='mandatory'><sup>*</sup></span></label><input type='text' name='Contact name' placeholder='First name' required><label for='first name'>Middle name:</label><input type='text' name='Contact name' placeholder='Middle name' required><label for='first name'>Last name:<span class='mandatory'><sup>*</sup></span></label><input type='text' name='Contact name' placeholder='Last name' required><label>Gender</label><select required><option value='m'>Male</option><option value='f'>Female</option><select/><p class='name- validation '>Please enter first name,middle name and last name using letters</p></div><div><label>Email</label><input type='email' required /><label for='Country name'>Country name<span class='mandatory'><sup>*</sup></span></label><select required class='country-name'></select><p class='name-validation'>Please enter first and last name using letters</p></div><div><label for='Contact number'>Contact number<span class='mandatory'><sup>*</sup></span></label><input type='number' name='Contact number' placeholder='Mobile number' required><p class='name- validation '>Please enter valid number </p></div></fieldset><fieldset id='personal-details'><h1>Personal Details</h1><div><label for='DateOfBirth'>Date Of Birth</label><input type='date' name='DOB'></div><div><h3>Address:</h3><fieldset style='overflow: auto'><label for='Address line 1'>Address line 1</label><input type='text' name='Address line 1' placeholder='Address line 1 '><label for='Address line 2'>Address line 2</label><input type='text' name='Address line 2' placeholder='Address line 2 '><label for='City'>City</label><input type='text' name='city name' placeholder='city'><label for='state'>State</label><input type='text' name='state name' placeholder='state'><label for='ZipCode'>Zip Code</label><input type='number' name='ZipCode' placeholder='Zip Code'><p class='name-validation'>Please enter valid 6-digits Code </p></fieldset></div></fieldset>";
+    var guestDetailsHtml = "<fieldset class='contact-details'> <h3>Contact Information</h3> <div> <label for='first name'>First name:<span class='mandatory'><sup>*</sup></span> </label> <input class='first-name' type='text' name='Contact name' placeholder='First name' required> <label for='first name'>Middle name:</label> <input class='second-name' type='text' name='Contact name' placeholder='Middle name' required> <label for='first name'>Last name:<span class='mandatory'><sup>*</sup></span> </label> <input class='last-name' type='text' name='Contact name' placeholder='Last name' required> <label>Gender</label> <select class='gender' required> <option value='m'>Male</option> <option value='f'>Female</option> <select/> <p class='name-validation '>Please enter first name,middle name and last name using letters</p></div><div> <label>Email</label> <input class='email' type='email' required/> <label for='Country name'>Country name<span class='mandatory'><sup>*</sup></span> </label> <select required class='country-name'></select> <p class='name-validation'>Please enter first and last name using letters</p></div><div> <label for='Contact number'>Contact number<span class='mandatory'><sup>*</sup></span> </label> <input class='contact-number' type='number' name='Contact number' placeholder='Mobile number' required> <p class='name- validation '>Please enter valid number </p></div></fieldset><fieldset class='personal-details'> <h1>Personal Details</h1> <div> <label for='DateOfBirth'>Date Of Birth</label> <input class='dob' type='date' name='DOB'> </div></fieldset>";
     var countriesOptions = "<option value='India'>India</option>";
     var guestCount;
 }
 function getAge(date)
 {
-    var calculatedAge = new Date().getFullYear() - (dob.split('-'))[0];
+    var calculatedAge = new Date().getFullYear() - (date.split('-'))[0];
     return calculatedAge;
 }
 function getGuestField()
@@ -17,76 +17,85 @@ function getGuestField()
     var guestForms = $(".guest-form-container");
     for (var guest = 0; guest < guestCount; guest++)
     {
-        var firstName = $(guestForms[guest]).children("fieldset:first-child>div:first-child>input:nth-child(1)").val();
-        var middleName = $(guestForms[guest]).children("fieldset:first-child>div:first-child>input:nth-child(2)").val();
-        var lastName = $(guestForms[guest]).children("fieldset:first-child>div:first-child>input:nth-child(3)").val();
-        var email = $(guestForms[guest]).children("fieldset:first-child>div:nth-child(2)>input:first-child").val();
-        var gender = $(guestForms[guest]).children("fieldset:first-child>div:first-child>select").val();
+        var firstName = $(guestForms[guest]).find(".first-name").val();
+        var middleName = $(guestForms[guest]).find(".second-name").val();
+        var lastName = $(guestForms[guest]).find(".last-name").val();
+        var email = $(guestForms[guest]).find(".email").val();
+        var gender = $(guestForms[guest]).find(".gender").val();
         var type;
-        var nameObject = "{\"FirstName\":" + firstName + "," + "\"MiddleName\":" + middleName + "," + "\"LastName\":" + lastName + "," + "}";
-        var dob;
-        var dateParts = $(guestForms[guest]).children("fieldset:nth-child(2)>div:first-child>input").val();
-        dateParts = dateParts.split("-");
-        dob = dateParts[2] + '-' + dateParts[0] + '-' + dateParts[1];
+        var nameObject = "{\"FirstName\":" +'"' +firstName+'"' + "," +"\"MiddleName\":" + '"'+middleName +'"'+ "," + "\"LastName\":" + '"'+lastName +'"'+ "}";
+        var dob=$(guestForms[guest]).find(".dob").val();
         var age = getAge(dob);
         if (age < 18) { type = "Child"; }
         else { type = "Adult"; }
         response +=
             "{\"Name\":" + nameObject +
-            "\"DateOfBirth\":" + dob +
-            "\"Age\":" + age +
-            "\"Email\":" + email +
-            "\"Gender\":" + gender +
-            "\"Type\":" + type +
-            +"},";
+            ',\"DateOfBirth\":' +'"'+ dob+'"' +
+            ",\"Age\":" +'"' +age + '"' +
+        ",\"Email\":" + '"' + email + '"' +
+        ",\"Gender\":" + '"' + gender + '"' +
+        ",\"Type\":" + '"' + type + '"'+
+            "},";
     }
     response = response.substring(0, response.length - 1);
     response += "]";
     return response;
 }
-
-function sendBookRequest()
-{
-    var HotelProductBookRQ =
+function sendBookRequest() {
+    var paymentDetails = $("#payment>input");
+    var addressParts = $("#address-info>input");
+    var summaryFields = $(".summary-field");
+    var priceDetails = summaryFields[8].value;
+    var HotelProductBookRQ = "{" +
+        '"CallerSessionId":' + '"' + sessionId + '"' + ',' +
+        '"Guests":' + getGuestField() + ',' +
+        '"PaymentDetails":' +
+        '{' +
+        '"Price":' + '"' + priceDetails + '"' + ',' +
+        '"BillingAddress":' +
+        '{' +
+        '"AddressLine1":' + '"' + addressParts[0].value + '"' + ',' +
+        '"AddressLine2":' + '"' + addressParts[1].value + '"' + ',' +
+        '"AddressContext":' + '"Address"' + ',' +
+        '"City":' + '"' + addressParts[2].value + '"' + ',' +
+        '"State":' + '"' + addressParts[3].value + '"' + ',' +
+        '"Country":' + '"India"' + ',' +
+        '"ZipCode":' + '"' + addressParts[4].value + '"' + ',' +
+        '"PhoneNumber":' + '"' + addressParts[5].value + '"' + '},' +
+        '"CreditCardDetails":' +
+        '{' +
+        '"NameOnCard":' + '"' + paymentDetails[0].value + '"' + ',' +
+        '"CardNumber":' + '"' + paymentDetails[1].value + '"' + ',' +
+        '"Cvv":' + '"' + paymentDetails[2].value + '"' + ',' +
+        '"Code":' + '"VI"' + ',' +
+        '"CardName":' + '"VISA"' + ',' +
+        '"Expiry":' + '"' + $("#Year").val() + "-" + $("#Month").val() + "-" + "01" + '"' + ',' +
+        '"IsThreeDAuth":' + '"true"' + '}' + '}' + '}';
+    var IEngineServiceRQ =
         {
-            "CallerSessionId": CallerSessionId,
-            "Guest": getGuestField(),
-            "PaymentDetails":
-            {
-                "Amount": ($("#booking-summary>input:nth-child(9)").val()).substring(0,-3),
-                "Currency": "USD",
-                "BillingAddress":
-                {
-                    "AddressLine1":$("#billing-address>input:nth-child(1)"),
-                    "AddressLine2": $("#billing-address>input:nth-child(2)"),
-                    "AddressContext": "Address",
-                    "City": $("#billing-address>input:nth-child(3)"),
-                    "State": $("#billing-address>input:nth-child(4)"),
-                    "Country": "India",
-                    "ZipCode": $("#billing-address>input:nth-child(5)"),
-                    "PhoneNumber": $("#billing-address>input:nth-child(6)"),
-                },
-                "CreditCardDetails":
-                {
-                    "NameOnCard": $("#payment>input:first-child").val(),
-                    "CardNumber": $("#payment>input:nth-child(2)").val(),
-                    "Cvv": $("#payment>input:nth-child(3)").val(),
-                    "Code": "VI",
-                    "CardName":"VISA",
-                    "Expiry": $("#Year").val() + "-"+$("#Month").val()+"-"+"01",
-                    "IsThreeDAuth": true
-                }
-            }
-    }
-
+            "ServiceName": "ProcessProductBooking",
+            "JsonRequest": HotelProductBookRQ
+        };
+    $.ajax({
+        type: 'post',
+        headers:
+        {
+            "Content-Type": "application/json"
+        },
+        url: "../padharojanab/value",
+        cache: false,
+        data: JSON.stringify(IEngineServiceRQ),
+        //beforeSend: showLoadingImage,
+        success: function (response) { console.log(response); },
+        error: function (response) { console.log("error :" + response); }
+    });
 }
-
 function initialiser() {
     
     summaryFieldsPlaceHolders = $(".summary-field");
     summaryFieldValues = (document.cookie).split(";");
     {
-        sessionId = summaryFieldValues[0];
+        sessionId = ((summaryFieldValues[0]).split("="))[1];
 
         summaryFieldsPlaceHolders[0].value = (summaryFieldValues[1].split("="))[1];
         $(summaryFieldsPlaceHolders[0]).attr('size', $(summaryFieldsPlaceHolders[0]).val().length);
--- SystemContracts/ConsumerContracts/HotelProductBookRQ.cs
+++ SystemContracts/ConsumerContracts/HotelProductBookRQ.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using SystemContracts.Attributes;
+using SystemContracts.ServiceContracts;
 
 namespace SystemContracts.ConsumerContracts
 {
-    public class HotelProductBookRQ
+    public class HotelProductBookRQ : IEngineServiceRQ
     {
         public string CallerSessionId { get; set; }
 
